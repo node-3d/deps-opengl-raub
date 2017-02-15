6754f4d842ca817faa5a53506ce929aa9c7bad45
Luis Blanco <raubtierxxx@gmail.com>
2017-02-15 17:34:04 +0300

    new interface

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict';
 
-module.exports = __dirname;
+const os = require('os');
+
+const platformPaths = {
+	win32 : '/bin_win32' ,
+	linux : '/bin_linux' ,
+	darwin: '/bin_darwin',
+};
+
+const binPath = __dirname + platformPaths[os.platform()];
+
+process.env.path += ';' + binPath;
+
+module.exports = {
+	root: __dirname,
+	bin : binPath,
+};
