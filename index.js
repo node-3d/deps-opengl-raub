3e853bdfca31456bc2e43b4dce34e933c1404092
Luis Blanco <raubtierxxx@gmail.com>
2018-01-20 19:52:46 +0300

    rename dirs fix index

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
 'use strict';
 
-
-const remDirs = {
-	bin_win32    : 1,
-	bin_win64    : 1,
-	bin_linux32  : 1,
-	bin_linux64  : 1,
-	bin_mac64    : 1,
-};
-
-const platformPaths = {
-	
-	get win32()  { return process.arch === 'x64' ? 'bin_win64'   : 'bin_win32';   },
-	get linux()  { return process.arch === 'x64' ? 'bin_linux64' : 'bin_linux32'; },
-	get darwin() { return 'bin_mac64'; },
-	
-};
-
-const binDir  = platformPaths[process.platform];
-delete remDirs[binDir];
-
-const binPath = `${__dirname}/${binDir}`;
-
-
-if (process.platform === 'win32') {
-	process.env.path = `${process.env.path ? `${process.env.path};` : ''}${binPath}`;
-}
-
-
-module.exports = {
-	bin     : binPath,
-	rem     : Object.keys(remDirs).map(k => `${__dirname.replace(/\\/g, '/')}/${k}`).join(' '),
-	include : `${__dirname}/include`,
-};
+module.exports = require('node-addon-tools-raub').paths(__dirname);
