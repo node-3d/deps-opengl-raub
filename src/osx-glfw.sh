da094ea634011ebfcf5a69577c46399955d7d845
Luis Blanco <luisblanco1337@gmail.com>
2019-08-21 14:30:13 +0300

    :hammer: Rebuild glfw

--- src/osx-glfw.sh
+++ src/osx-glfw.sh
@@ -3,10 +3,10 @@ echo 'GLFW Build Started'
 (
 	
 	cd src
-	unzip -qq glfw-3.2.1.zip -d .
+	unzip -qq glfw-3.3.zip -d .
 	
 	(
-		cd glfw-3.2.1
+		cd glfw-3.3
 		
 		cmake \
 			-DBUILD_SHARED_LIBS=ON \
@@ -21,7 +21,7 @@ echo 'GLFW Build Started'
 			-DCMAKE_SHARED_LINKER_FLAGS='-install_name "@rpath/glfw.dylib"' \
 			.
 		
-		echo '/Applications/Xcode-9.4.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang  -dynamiclib -Wl,-headerpad_max_install_names -install_name "@rpath/glfw.dylib" -compatibility_version 3.0.0 -current_version 3.2.0 -o libglfw.3.2.dylib CMakeFiles/glfw.dir/context.c.o CMakeFiles/glfw.dir/init.c.o CMakeFiles/glfw.dir/input.c.o CMakeFiles/glfw.dir/monitor.c.o CMakeFiles/glfw.dir/vulkan.c.o CMakeFiles/glfw.dir/window.c.o CMakeFiles/glfw.dir/cocoa_init.m.o CMakeFiles/glfw.dir/cocoa_joystick.m.o CMakeFiles/glfw.dir/cocoa_monitor.m.o CMakeFiles/glfw.dir/cocoa_window.m.o CMakeFiles/glfw.dir/cocoa_time.c.o CMakeFiles/glfw.dir/posix_tls.c.o CMakeFiles/glfw.dir/nsgl_context.m.o -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo' \
+		echo '/Applications/Xcode-9.4.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang  -dynamiclib -Wl,-headerpad_max_install_names -install_name "@rpath/glfw.dylib" -compatibility_version 3.0.0 -current_version 3.3.0 -o libglfw.3.3.dylib CMakeFiles/glfw.dir/context.c.o CMakeFiles/glfw.dir/init.c.o CMakeFiles/glfw.dir/input.c.o CMakeFiles/glfw.dir/monitor.c.o CMakeFiles/glfw.dir/vulkan.c.o CMakeFiles/glfw.dir/window.c.o CMakeFiles/glfw.dir/cocoa_init.m.o CMakeFiles/glfw.dir/cocoa_joystick.m.o CMakeFiles/glfw.dir/cocoa_monitor.m.o CMakeFiles/glfw.dir/cocoa_window.m.o CMakeFiles/glfw.dir/cocoa_time.c.o CMakeFiles/glfw.dir/posix_tls.c.o CMakeFiles/glfw.dir/nsgl_context.m.o -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo' \
 		> src/CMakeFiles/glfw.dir/link.txt
 		
 		make
@@ -29,7 +29,7 @@ echo 'GLFW Build Started'
 	)
 	
 	
-	mv glfw-3.2.1/src/libglfw.3.2.dylib glfw.dylib
+	mv glfw-3.3/src/libglfw.3.3.dylib glfw.dylib
 	
 )
 
