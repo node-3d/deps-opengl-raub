428f428333f18b260ae35249461f6f44abcff9c1
Luis Blanco <luisblanco1337@gmail.com>
2019-08-23 09:41:32 +0300

    :hammer: Try osx build

--- src/osx-glfw.sh
+++ src/osx-glfw.sh
@@ -23,14 +23,14 @@ echo 'GLFW Build Started'
 			-DCMAKE_SHARED_LINKER_FLAGS='-install_name "@rpath/glfw.dylib"' \
 			.
 		
-		echo '/Applications/Xcode-9.4.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang  -dynamiclib -Wl,-headerpad_max_install_names -install_name "@rpath/glfw.dylib" -compatibility_version 3.0.0 -current_version 3.3.0 -o libglfw.3.3.dylib CMakeFiles/glfw.dir/context.c.o CMakeFiles/glfw.dir/init.c.o CMakeFiles/glfw.dir/input.c.o CMakeFiles/glfw.dir/monitor.c.o CMakeFiles/glfw.dir/vulkan.c.o CMakeFiles/glfw.dir/window.c.o CMakeFiles/glfw.dir/cocoa_init.m.o CMakeFiles/glfw.dir/cocoa_joystick.m.o CMakeFiles/glfw.dir/cocoa_monitor.m.o CMakeFiles/glfw.dir/cocoa_window.m.o CMakeFiles/glfw.dir/cocoa_time.c.o CMakeFiles/glfw.dir/posix_tls.c.o CMakeFiles/glfw.dir/nsgl_context.m.o -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo' \
-		> src/CMakeFiles/glfw.dir/link.txt
+		# echo '/Applications/Xcode-9.4.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang  -dynamiclib -Wl,-headerpad_max_install_names -install_name "@rpath/glfw.dylib" -compatibility_version 3.0.0 -current_version 3.3.0 -o libglfw.3.3.dylib CMakeFiles/glfw.dir/context.c.o CMakeFiles/glfw.dir/init.c.o CMakeFiles/glfw.dir/input.c.o CMakeFiles/glfw.dir/monitor.c.o CMakeFiles/glfw.dir/vulkan.c.o CMakeFiles/glfw.dir/window.c.o CMakeFiles/glfw.dir/cocoa_init.m.o CMakeFiles/glfw.dir/cocoa_joystick.m.o CMakeFiles/glfw.dir/cocoa_monitor.m.o CMakeFiles/glfw.dir/cocoa_window.m.o CMakeFiles/glfw.dir/cocoa_time.c.o CMakeFiles/glfw.dir/posix_tls.c.o CMakeFiles/glfw.dir/nsgl_context.m.o -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo' \
+		# > src/CMakeFiles/glfw.dir/link.txt
 		
 		make
 		
 	)
 	
-	
+	mv glfw-3.3/src/CMakeFiles/glfw.dir/link.txt build/link.txt
 	mv glfw-3.3/src/libglfw.3.3.dylib build/glfw.dylib
 	
 )
