99a0280d9363a6ffd2b7906209afc364374ecf81
Luis Blanco <luisblanco1337@gmail.com>
2021-04-20 22:21:14 +0300

    Update TS and CI

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,12 @@
-declare const paths: Readonly<{
-    bin: string;
-    include: string;
-}>;
-
-export default paths;
+declare module "deps-opengl-raub" {
+	/**
+	 * Path to binaries
+	 * Platform binary directory absolute path
+	*/
+	export const bin: string;
+	/**
+	 * Path to includes
+	 * Include directory for this module
+	*/
+	export const include: string;
+}
