25dcf76dd0080e7f483225e3cd4f2b4eca5d9530
Luis Blanco <luisblanco1337@gmail.com>
2023-01-04 22:47:59 +0400

    Adjust windows scripts

--- src/windows-glew.sh
+++ src/windows-glew.sh
@@ -1,6 +1,7 @@
 (
 	cd glew-2.1.0/build/vc12
-	msbuild /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v142 glew_shared.vcxproj
+	echo $PATH
+	msbuild.exe /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v142 glew_shared.vcxproj
 )
 
 mv glew-2.1.0/bin/Release/x64/glew32.dll build/glew32.dll
