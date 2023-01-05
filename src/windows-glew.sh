009d61377422c611d589345aae0ba439fceda42b
Luis Blanco <luisblanco1337@gmail.com>
2023-01-05 16:28:43 +0400

    Fix msbuild case

--- src/windows-glew.sh
+++ src/windows-glew.sh
@@ -1,7 +1,6 @@
 (
 	cd glew-2.1.0/build/vc12
-	echo $PATH
-	msbuild /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v142 glew_shared.vcxproj
+	MSBuild /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v142 glew_shared.vcxproj
 )
 
 mv glew-2.1.0/bin/Release/x64/glew32.dll build/glew32.dll
