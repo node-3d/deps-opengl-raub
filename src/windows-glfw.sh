adce72e34fc078fc69b10d561e6f51a8443e36fb
Luis Blanco <luisblanco1337@gmail.com>
2023-01-05 13:33:31 +0400

    Fix yml condition

--- src/windows-glfw.sh
+++ src/windows-glfw.sh
@@ -1,7 +1,7 @@
 (
 	cd glfw-3.3.8
 	cmake -DBUILD_SHARED_LIBS=ON -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DGLFW_VULKAN_STATIC=OFF .
-	msbuild.exe /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v142 ALL_BUILD.vcxproj
+	msbuild /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v142 ALL_BUILD.vcxproj
 )
 
 mv glfw-3.3.8/src/Release/glfw3.dll build/glfw3.dll
