03d1603023e1d9df7b40cac493e5e46934f7bddf
Luis Blanco <luisblanco1337@gmail.com>
2023-01-05 22:14:37 +0400

    Adjust windows build 7

--- src/extract.sh
+++ src/extract.sh
@@ -1,7 +1,7 @@
-rm -rf glew-2.1.0
-unzip -qq glew-2.1.0.zip -d .
+rm -rf src/glew-2.1.0
+unzip -qq src/glew-2.1.0.zip -d src
 
-rm -rf glfw-3.3.8
-unzip -qq glfw-3.3.8.zip -d .
+rm -rf src/glfw-3.3.8
+unzip -qq src/glfw-3.3.8.zip -d src
 
-mkdir -p build
+mkdir -p src/build
