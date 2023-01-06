adc997d0600fb5ebbab5ef845293a5cacf660c1e
Luis Blanco <luisblanco1337@gmail.com>
2023-01-06 22:36:04 +0400

    Try finding so libs 2

--- src/build-post.js
+++ src/build-post.js
@@ -20,12 +20,37 @@ const fail = (error) => {
 };
 
 
+const findLinuxLib = async (name) => {
+	try {
+		const { stdout } = await exec(`ldconfig -p | grep ${name}`);
+		if (!stdout.includes(name)) {
+			return;
+		}
+		const matched = stdout.match(/ => \/.*/);
+		if (!matched) {
+			return;
+		}
+		const libPath = mathced[0].replace(' => ', '');
+		console.log('COPY', libPath, `${binPath}/${name}`);
+		await copy(libPath, `${binPath}/${name}`);
+	} catch (error) {
+		fail(error);
+	}
+};
+
+
 (async () => {
 	try {
 		await ensuredir(binPath);
 		
 		if (platform === 'windows') {
 			await copy(path.resolve('src/OpenGL32.Lib'), `${binPath}/OpenGL32.Lib`);
+		} else if (platform === 'linux') {
+			await findLinuxLib('libGL.so');
+			await findLinuxLib('libXrandr.so');
+			await findLinuxLib('libXrender.so');
+			// await findLinuxLib('libXrandr.a');
+			// await findLinuxLib('libXrender.a');
 		}
 		
 		await copyall(path.resolve('src/build'), binPath);
