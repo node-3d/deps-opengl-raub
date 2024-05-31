5d6cdbf57552786d296fa24b11586921845ecef1
Luis Blanco <luisblanco1337@gmail.com>
2024-05-31 20:29:15 +0400

    Adjust build sh 10

--- src/update-aarch64.sh
+++ src/update-aarch64.sh
@@ -1,2 +1,9 @@
+>&2 echo YUM START
 sudo yum -y update
-sudo yum -y install cmake mesa-libGL-devel libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel wayland libxkbcommon
+>&2 echo YUM UPDATE DONE
+sudo yum -y --nobest --allowerasing install tuned tuned-profiles-oci
+>&2 echo YUM TUNED DONE
+sudo yum -y --nobest --allowerasing install cmake mesa-libGL-devel libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
+>&2 echo YUM SAFE DONE
+sudo yum -y --nobest --allowerasing install wayland-devel libxkbcommon-x11-devel
+>&2 echo YUM WAYLAND DONE
