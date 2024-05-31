726cbe9a34fb486192079ffb6b8eb2685b73fc3c
Luis Blanco <luisblanco1337@gmail.com>
2024-05-31 18:00:27 +0400

    Adjust build sh 4

--- src/update-aarch64.sh
+++ src/update-aarch64.sh
@@ -1,2 +1,2 @@
 sudo yum -y update
-sudo yum -y install cmake mesa-libGL-devel libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
+sudo yum -y install cmake mesa-libGL-devel libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel wayland-devel
