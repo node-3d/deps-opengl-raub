174f23bd3e3df249735a4bbb6cd456519eb4ad69
Luis Blanco <luisblanco1337@gmail.com>
2024-05-31 20:43:27 +0400

    Adjust build sh 11

--- src/windows-glew.cmd
+++ src/windows-glew.cmd
@@ -1,4 +1,4 @@
 msbuild /p:Platform=x64 /p:Configuration=Release /p:PlatformToolset=v142 src\glew\build\vc12\glew_shared.vcxproj
 
-copy /y src\glew-2.2.0\bin\Release\x64\glew32.dll src\build\glew32.dll
-copy /y src\glew-2.2.0\lib\Release\x64\glew32.lib src\build\glew32.lib
+copy /y src\glew\bin\Release\x64\glew32.dll src\build\glew32.dll
+copy /y src\glew\lib\Release\x64\glew32.lib src\build\glew32.lib
