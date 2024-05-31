204a1b784a7eabbc17668ff90b2e15cc7b63129e
Luis Blanco <luisblanco1337@gmail.com>
2024-05-31 13:49:29 +0400

    Use glew release snapshot

--- src/extract.sh
+++ src/extract.sh
@@ -2,11 +2,12 @@
 	cd src
 	
 	rm -rf build
-	mkdir -p build
-	
 	rm -rf glew
-	git clone --depth 1 -b glew-2.2.0 https://github.com/nigels-com/glew.git
-	
 	rm -rf glfw
+	
+	mkdir -p build
 	git clone --depth 1 -b 3.4 https://github.com/glfw/glfw.git
 )
+
+unzip -qq src/glew-2.2.0.zip -d src
+mv src/glew-2.2.0 src/glew
