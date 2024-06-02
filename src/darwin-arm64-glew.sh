1d556b3f02306763e6db93f834fa87ebedd480d5
Luis Blanco <luisblanco1337@gmail.com>
2024-06-02 10:49:01 +0400

    Adjust mac arm build

--- src/darwin-arm64-glew.sh
+++ src/darwin-arm64-glew.sh
@@ -2,7 +2,7 @@
 	cd src
 	(
 		cd glew
-		make LDFLAGS.EXTRA='-install_name "@rpath/glew.dylib" -arch x86_64' glew.lib
+		make LDFLAGS.EXTRA='-install_name "@rpath/glew.dylib" -arch arm64' glew.lib
 	)
 	
 	mv glew/lib/libGLEW.2.2.0.dylib build/glew.dylib
