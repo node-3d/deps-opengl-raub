bcaf76016ce088e4c64d800783e59ecc3d7aa660
Luis Blanco <luisblanco1337@gmail.com>
2025-02-21 13:56:17 +0400

    Try gh arm linux 3

--- src/update-aarch64.sh
+++ src/update-aarch64.sh
@@ -1,4 +1,6 @@
-sudo yum -y update
-sudo yum -y --nobest --allowerasing install tuned tuned-profiles-oci
-sudo yum -y --nobest --allowerasing install cmake mesa-libGL-devel libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
-sudo yum -y --nobest --allowerasing install wayland-devel libxkbcommon-devel
+chmod +x src/update-linux.sh && src/update-linux.sh
+
+# sudo yum -y update
+# sudo yum -y --nobest --allowerasing install tuned tuned-profiles-oci
+# sudo yum -y --nobest --allowerasing install cmake mesa-libGL-devel libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
+# sudo yum -y --nobest --allowerasing install wayland-devel libxkbcommon-devel
