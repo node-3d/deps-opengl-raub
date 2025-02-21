303cf16b6ae864e70ba46c2cfe37b45b1faf285b
Luis Blanco <luisblanco1337@gmail.com>
2025-02-21 14:03:56 +0400

    Rmove draft flag 2

--- src/update-aarch64.sh
+++ src/update-aarch64.sh
@@ -1,6 +1,1 @@
 chmod +x src/update-linux.sh && src/update-linux.sh
-
-# sudo yum -y update
-# sudo yum -y --nobest --allowerasing install tuned tuned-profiles-oci
-# sudo yum -y --nobest --allowerasing install cmake mesa-libGL-devel libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
-# sudo yum -y --nobest --allowerasing install wayland-devel libxkbcommon-devel
